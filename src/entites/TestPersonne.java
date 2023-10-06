73b8e2e975aebf688f68ed7059e0b59acd906126
Djimy Drouet <drouet.djimy@gmail.com>
2023-10-06 14:26:44 +0200

    TP3 Instances et Methodes

--- src/entites/TestPersonne.java
+++ src/entites/TestPersonne.java
@@ -8,11 +8,16 @@ public class TestPersonne {
 		AdressePostale adresseDjimy = new AdressePostale(306,"Le moulin du pont",49600,"Beaupreau en mauges");
 		Personne Djimy = new Personne("Drouet","Djimy",adresseDjimy);
 		
+		Djimy.setNom("Georges");
+		Djimy.setPrenom("Lucas");
+		Djimy.setAdresse(adresseDjimy);
+		
 		AdressePostale adresseBastien = new AdressePostale(24,"Rue de périgné",17000,"La rochelle");
 		Personne Bastien = new Personne("Rollat","Bastien",adresseBastien);
 		
-		System.out.println("Affichage de Djimy : " + Djimy);
-		System.out.println("Affichage de Bastien : " + Bastien);
+		Bastien.setNom("darth");
+		Bastien.setPrenom("Vador");
+		Bastien.setAdresse(adresseBastien);
 
 	}
 }
--- src/entites2/Personne.java
+++ src/entites2/Personne.java
@@ -1,6 +1,6 @@
 package entites2;
 
-import entites.AdressePostale;
+import entites.*;
 
 public class Personne {
 	
@@ -14,4 +14,29 @@ public class Personne {
 		this.prenom = prenom;
 		this.adresse = adresse;
 	}
+	public void getPrenomNom() {
+		System.out.println("Prénom : "+prenom);
+		System.out.println("nom : "+nom);
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public AdressePostale getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(AdressePostale adresse) {
+		this.adresse = adresse;
+	}
+	
+	
+	
 }
